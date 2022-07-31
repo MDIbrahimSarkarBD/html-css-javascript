463d075abf0e0849ae0fc1aae9fcee7b32d62ae4
MDIbrahimSarkarBD <ibrahimcomputer733@gmail.com>
2022-07-31 17:19:58 +0600

    theme customize

--- js/ibrahim.js
+++ js/ibrahim.js
@@ -1,3 +1,10 @@
-class ibrahim{
-    
+class IS_Date{
+    dates = new Date();
+    y = this.dates.getFullYear();
+    m = this.dates.getMonth(); 
+    d = this.dates.getDate(); 
+    fullDate = "";
+    constructor() {
+        this.fullDate =  this.y+"/"+ this.m +"/" + this.d;
+    }
 }
